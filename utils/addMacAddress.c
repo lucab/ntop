dd319a5facd68a5a172479dd2865d222a8039efa
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2000-10-18 12:49:40 +0000

    Miro changes.

--- utils/addMacAddress.c
+++ utils/addMacAddress.c
@@ -1,8 +1,7 @@
 /*
- *  Copyright (C) 1998-99 Luca Deri <deri@unipi.it>
+ *  Copyright (C) 1998-2000 Luca Deri <deri@ntop.org>
  *                      
- *		  	  Centro SERRA, University of Pisa
- *		 	  http://www-serra.unipi.it/
+ *		 	  http://www.ntop.org/
  *  					
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
