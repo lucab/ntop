1f5cb6600f99bdc819144a7a5505a57ae019aecb
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2002-05-19 19:35:35 +0000

    Changed logic for hash extension: the hash does not double its size but it is extended with not more than 4096 buckets. This avoids to have large hashes

--- hash.c
+++ hash.c
@@ -652,8 +652,11 @@ u_int getHostInfo(struct in_addr *hostIpAddress,
 	list->idx = myGlobals.device[actualDeviceId].actualHashSize;
 	if(myGlobals.device[actualDeviceId].actualHashSize < 512)
 	  myGlobals.device[actualDeviceId].actualHashSize = 512;
+	else if(myGlobals.device[actualDeviceId].actualHashSize >= 4096)
+	  myGlobals.device[actualDeviceId].actualHashSize += 4096;
 	else
 	  myGlobals.device[actualDeviceId].actualHashSize *= 2; /* Double */
+
 	sz = myGlobals.device[actualDeviceId].actualHashSize*sizeof(struct hostTraffic*);
 	myGlobals.device[actualDeviceId].hash_hostTraffic = (struct hostTraffic**)realloc(myGlobals.device[actualDeviceId].hash_hostTraffic, sz);
 	memset(&myGlobals.device[actualDeviceId].hash_hostTraffic[list->idx],
