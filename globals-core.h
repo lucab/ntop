8299f5045964b0726a4f2b992ac410a3fb098b77
burton <burton@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2002-06-12 23:32:39 +0000

    Remove non-existent functions from extern definitions for ntop.c
    -----Burton

--- globals-core.h
+++ globals-core.h
@@ -158,11 +158,8 @@ extern void detachFromTerminal(void);
 extern void handleProtocols();
 extern void addDefaultProtocols(void);
 extern int mapGlobalToLocalIdx(int port);
-extern void *updateThptLoop(void *notUsed);
-extern void* updateHostTrafficStatsThptLoop(void* notUsed);
 extern void *updateDBHostsTrafficLoop(void* notUsed);
 extern void *scanIdleLoop(void *notUsed);
-extern void *scanIdleSessionsLoop(void *notUsed);
 extern void createPortHash();
 #ifndef WIN32
 extern void *periodicLsofLoop(void *notUsed);
