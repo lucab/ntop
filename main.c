b921d58694a36d175fc5e0b824edc60a88afb6b5
burton <burton@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2003-08-12 21:11:26 +0000

    Add log messages for -t 5 debugging of getopt_long problems, if we have 'em
    -----Burton (ref 324)

--- main.c
+++ main.c
@@ -341,8 +341,10 @@ static int parseOptions(int argc, char* argv []) {
    * Parse command line options to the application via standard system calls
    */
 #ifdef HAVE_GETOPT_LONG
+  traceEvent(CONST_TRACE_NOISY, "NOTE: Calling getopt_long to process parameters");
   while((opt = getopt_long(argc, argv, theOpts, long_options, &opt_index)) != EOF) {
 #else
+  traceEvent(CONST_TRACE_NOISY, "NOTE: Calling getopt to process parameters");
   while((opt = getopt(argc, argv, theOpts)) != EOF) {
 #endif
 #ifdef PARAM_DEBUG
