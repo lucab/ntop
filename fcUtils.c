8f5260c438aae82fe9db0b29379010e80dde78f5
dinesh <dinesh@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2003-12-22 18:50:56 +0000

    New files with support routines and defines for FibreChannel and SCSI.

---
+++ fcUtils.c
@@ -0,0 +1,470 @@
+/**
+ * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+ *                          http://www.ntop.org
+ *
+ * Copyright (C) 1998-2002 Luca Deri <deri@ntop.org>
+ *
+ * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+
+#include "ntop.h"
+#include <stdarg.h>
+#include <assert.h>
+#include "fcUtils.h"
+
+#ifdef MEMORY_DEBUG
+#include "leaks.h"
+#endif
+
+#ifdef CFG_MULTITHREADED
+static char stateChangeMutexInitialized = 0;
+static pthread_mutex_t stateChangeMutex;
+#endif
+
+static SessionInfo *passiveSessions;
+static u_short passiveSessionsLen;
+
+int isFlogiAcc (FcAddress *fcAddress, u_int8_t r_ctl, u_int8_t type, u_int8_t cmd)
+{
+
+    if (((fcAddress->domain == 0xFF) && (fcAddress->area == 0xFF) && (fcAddress->port == 0xFE)) && 
+        ((r_ctl & 0xF0) == FC_RCTL_ELS) && (type == FC_TYPE_ELS) &&
+        (cmd == FC_ELS_CMD_ACC)) {
+        return (TRUE);
+    }
+    else {
+        return (FALSE);
+    }
+}
+
+/* Routine to extract WWN from FLOGI frame */
+int fillFcHostInfo (const u_char *bp, HostTraffic *srcHost)
+{
+    assert (bp != NULL);
+
+    srcHost->fcRecvSize = ntohs ((u_short)bp[10] & 0xFFF);
+    memcpy (srcHost->pWWN.str, &bp[20], LEN_WWN_ADDRESS);
+    memcpy (srcHost->nWWN.str, &bp[28], LEN_WWN_ADDRESS);
+
+    return (0);
+}
+
+/* Routine to extract WWN from RDI frame */
+
+/* Routine to extract information from dNS registration messages */
+
+
+/* Routine to extract WWN from PLOGI frame */
+int isPlogi (u_int8_t r_ctl, u_int8_t type, u_int8_t cmd)
+{
+    if (((r_ctl & 0xF0) == FC_RCTL_ELS) && (type == FC_TYPE_ELS) &&
+        (cmd == FC_ELS_CMD_PLOGI)) {
+        return (TRUE);
+    }
+    else {
+        return (FALSE);
+    }
+}
+
+int isLogout (u_int8_t r_ctl, u_int8_t type, u_int8_t cmd)
+{
+    if (((r_ctl & 0xF0) == FC_RCTL_ELS) && (type == FC_TYPE_ELS) &&
+        (cmd == FC_ELS_CMD_LOGO)) {
+        return (TRUE);
+    }
+    else {
+        return (FALSE);
+    }
+}
+
+int isRscn (u_int8_t r_ctl, u_int8_t type, u_int8_t cmd)
+{
+    if (((r_ctl & 0xF0) == FC_RCTL_ELS) && (type == FC_TYPE_ELS) &&
+        (cmd == FC_ELS_CMD_RSCN)) {
+        return (TRUE);
+    }
+    else {
+        return (FALSE);
+    }
+}
+
+int fillFcpInfo (const u_char *bp, HostTraffic *srcHost, HostTraffic *dstHost)
+{
+    int offset = 0;
+    u_int32_t fcpDl;
+    
+    assert (bp != NULL);
+    assert (srcHost != NULL);
+    assert (dstHost != NULL);
+    
+    if (bp[offset] == 0) {
+        /* This is a single-level LUN */
+    }
+
+    fcpDl = ntohl (*(u_int32_t *)&bp[offset+28]);
+
+    if (bp[offset+11] & 0x1) {
+        incrementTrafficCounter (&srcHost->scsiWriteBytes, fcpDl);
+        incrementTrafficCounter (&dstHost->scsiWriteBytes, fcpDl);
+    }
+    else if (bp[offset+11] & 0x2) {
+        incrementTrafficCounter (&srcHost->scsiReadBytes, fcpDl);
+        incrementTrafficCounter (&dstHost->scsiReadBytes, fcpDl);
+    }
+
+    return (0);
+}
+
+FcFabricElementHash *getFcFabricElementHash (u_short vsanId, int actualDeviceId)
+{
+    FcFabricElementHash **theHash;
+    u_int myIdx = 0, idx;
+
+    idx = vsanId % MAX_ELEMENT_HASH;
+    theHash = myGlobals.device[actualDeviceId].vsanHash;    
+
+    while(1) {
+        if((theHash[idx] == NULL) || (theHash[idx]->vsanId == vsanId))
+            break;
+
+        idx = (idx+1) % MAX_ELEMENT_HASH;
+        if(++myIdx == MAX_ELEMENT_HASH) {
+            traceEvent(CONST_TRACE_WARNING, "updateElementHash(): hash full!");
+            return (NULL);
+        }
+    }
+
+    if(theHash[idx] == NULL) {
+        theHash[idx] = (FcFabricElementHash*) calloc(1, sizeof(FcFabricElementHash));
+        theHash[idx]->vsanId = vsanId;
+    }
+
+    return (theHash[idx]);
+}
+
+int isValidFcNxPort (FcAddress *fcAddress)
+{
+    if (fcAddress != NULL) {
+        if ((fcAddress->domain < MAX_FC_DOMAINS) && (fcAddress->domain))
+            return (TRUE);
+    }
+
+    return (FALSE);
+}
+
+int updateFcFabricElementHash (FcFabricElementHash **theHash, u_short vsanId,
+                               const u_char *bp, FcAddress *srcAddr, FcAddress *dstAddr,
+                               u_short protocol, u_char r_ctl, u_int32_t pktlen)
+{
+    u_int myIdx = 0, idx;
+    FcFabricElementHash *hash;
+    u_int8_t cmd, srcDomain, dstDomain;
+    u_short payload_len;
+    u_int8_t gs_type, gs_stype;
+
+    idx = vsanId % MAX_ELEMENT_HASH;
+    
+    while(1) {
+        if((theHash[idx] == NULL) || (theHash[idx]->vsanId == vsanId))
+            break;
+
+        idx = (idx+1) % MAX_ELEMENT_HASH;
+        if(++myIdx == MAX_ELEMENT_HASH) {
+            traceEvent(CONST_TRACE_WARNING, "updateElementHash(): hash full!");
+            return (1);
+        }
+    }
+
+    if(theHash[idx] == NULL) {
+        theHash[idx] = (FcFabricElementHash*) calloc(1, sizeof(FcFabricElementHash));
+        theHash[idx]->vsanId = vsanId;
+    }
+
+    /* ************************** */
+
+    hash = theHash[idx];
+
+    incrementTrafficCounter (&hash->totBytes, pktlen);
+    incrementTrafficCounter (&hash->totPkts, 1);
+
+#ifdef NOT_YET    
+    if (protocol == FC_FTYPE_SWILS) {
+        cmd = *bp;
+
+        switch (cmd) {
+        case FC_SWILS_ELP:
+            incrementTrafficCounter (&hash->pmBytes, pktlen);
+            incrementTrafficCounter (&hash->pmPkts, 1);
+            hash->fabricConfStartTime = myGlobals.actTime;
+
+            break;
+        case FC_SWILS_ESC:
+            incrementTrafficCounter (&hash->pmBytes, pktlen);
+            incrementTrafficCounter (&hash->pmPkts, 1);
+            
+            break;
+        case FC_SWILS_BF:
+            incrementTrafficCounter (&hash->dmBytes, pktlen);
+            incrementTrafficCounter (&hash->dmPkts, 1);
+            incrementTrafficCounter (&hash->numBF, 1);
+            hash->fabricConfStartTime = myGlobals.actTime;
+
+            break;
+        case FC_SWILS_RCF:
+            incrementTrafficCounter (&hash->dmBytes, pktlen);
+            incrementTrafficCounter (&hash->dmPkts, 1);
+            incrementTrafficCounter (&hash->numRCF, 1);
+            hash->fabricConfStartTime = myGlobals.actTime;
+            
+            break; 
+        case FC_SWILS_EFP:
+            incrementTrafficCounter (&hash->dmBytes, pktlen);
+            incrementTrafficCounter (&hash->dmPkts, 1);
+
+            /* Copy the latest EFP for the domain list */
+            payload_len = ntohs (*(u_short *)&bp[2]);
+            memcpy (hash->principalSwitch.str, &bp[8], sizeof (wwn_t));
+
+            payload_len -= 16;
+            payload_len = (payload_len > pktlen) ? pktlen : payload_len;
+            
+            hash->domainList = (FcDomainList *)malloc (payload_len);
+            memcpy (hash->domainList, &bp[16], payload_len);
+
+            hash->domainListLen = payload_len;
+
+            break;
+        case FC_SWILS_DIA:
+            incrementTrafficCounter (&hash->dmBytes, pktlen);
+            incrementTrafficCounter (&hash->dmPkts, 1);
+
+            break;
+        case FC_SWILS_RDI:
+            incrementTrafficCounter (&hash->dmBytes, pktlen);
+            incrementTrafficCounter (&hash->dmPkts, 1);
+            
+            break;
+        case FC_SWILS_HLO:
+            incrementTrafficCounter (&hash->fspfBytes, pktlen);
+            incrementTrafficCounter (&hash->fspfPkts, 1);
+            incrementTrafficCounter (&hash->hloPkts, 1);
+            
+            break;
+        case FC_SWILS_LSU:
+            incrementTrafficCounter (&hash->fspfBytes, pktlen);
+            incrementTrafficCounter (&hash->fspfPkts, 1);
+            incrementTrafficCounter (&hash->lsuBytes, pktlen);
+            incrementTrafficCounter (&hash->lsuPkts, 1);
+            
+            break;
+        case FC_SWILS_LSA:
+            incrementTrafficCounter (&hash->fspfBytes, pktlen);
+            incrementTrafficCounter (&hash->fspfPkts, 1);
+            incrementTrafficCounter (&hash->lsaBytes, pktlen);
+            incrementTrafficCounter (&hash->lsaPkts, 1);
+
+            /* Check if fabric configuration is over */
+            if (hash->fabricConfInProgress) {
+                flags = bp[23];
+
+                if (flags & 0x2) {
+                    /* Compute duration of fabric configuration, avg */
+                    confTime = difftime (myGlobals.actTime, hash->fabricConfStartTime);
+
+                    if (hash->maxTimeFabricConf < confTime) {
+                        hash->maxTimeFabricConf = confTime;
+                    }
+
+                    if (hash->minTimeFabricConf > confTime) {
+                        hash->minTimeFabricConf = confTime;
+                    }
+                }
+            }
+            
+            break;
+        case FC_SWILS_RSCN:
+            incrementTrafficCounter (&hash->rscnBytes, pktlen);
+            incrementTrafficCounter (&hash->rscnPkts, 1);
+
+            break;
+        case FC_SWILS_DRLIR:
+            break;
+        case FC_SWILS_DSCN:
+            break;
+        case FC_SWILS_LOOPD:
+            break;
+            
+        case FC_SWILS_MR:
+            incrementTrafficCounter (&hash->zsBytes, pktlen);
+            incrementTrafficCounter (&hash->zsPkts, 1);
+            
+            break;
+        case FC_SWILS_ACA:
+            incrementTrafficCounter (&hash->zsBytes, pktlen);
+            incrementTrafficCounter (&hash->zsPkts, 1);
+
+            hash->zoneConfStartTime = myGlobals.actTime;
+            hash->zoneConfInProgress = TRUE;
+            
+            break;
+        case FC_SWILS_RCA:
+            incrementTrafficCounter (&hash->zsBytes, pktlen);
+            incrementTrafficCounter (&hash->zsPkts, 1);
+
+            /* We should in reality do this when we see the ACC for RCA */
+            if (hash->zoneConfInProgress) {
+                hash->zoneConfInProgress = FALSE;
+
+                confTime = difftime (myGlobals.actTime, hash->zoneConfStartTime);
+                if (hash->maxTimeZoneConf < confTime) {
+                    hash->maxTimeZoneConf = confTime;
+                }
+                
+                if (hash->minTimeZoneConf > confTime) {
+                    hash->minTimeZoneConf = confTime;
+                }
+            }
+            break;
+        case FC_SWILS_SFC:
+            incrementTrafficCounter (&hash->zsBytes, pktlen);
+            incrementTrafficCounter (&hash->zsPkts, 1);
+            incrementTrafficCounter (&hash->numZoneConf, 1);
+            
+            break;
+        case FC_SWILS_UFC:
+            incrementTrafficCounter (&hash->zsBytes, pktlen);
+            incrementTrafficCounter (&hash->zsPkts, 1);
+            
+            break;
+        case FC_SWILS_SWACC:
+        case FC_SWILS_SWRJT:
+            break;
+        default:
+            traceEvent (CONST_TRACE_ALWAYSDISPLAY, "updateFcFabricElementHash: Unknown SW_ILS command %d\n",
+                        cmd);
+            break;
+        }
+    }
+    else {
+    }
+#else        
+    if (protocol == FC_FTYPE_SWILS) {
+        cmd = *bp;
+
+        switch (cmd) {
+        case FC_SWILS_ELP:
+        case FC_SWILS_BF:
+        case FC_SWILS_RCF:
+            hash->fabricConfStartTime = myGlobals.actTime;
+            break;
+        case FC_SWILS_EFP:
+            /* Copy the latest EFP for the domain list */
+            payload_len = ntohs (*(u_short *)&bp[2]);
+            memcpy (hash->principalSwitch.str, &bp[8], sizeof (wwn_t));
+
+            payload_len -= 16;
+            payload_len = (payload_len > pktlen) ? pktlen : payload_len;
+
+            if (hash->domainList != NULL) {
+                free (hash->domainList);
+                hash->domainList = NULL;
+            }
+            hash->domainList = (FcDomainList *)malloc (payload_len);
+            memcpy (hash->domainList, &bp[16], payload_len);
+
+            hash->domainListLen = payload_len;
+
+            break;
+        case FC_SWILS_LSA:
+            break;
+            
+        case FC_SWILS_ACA:
+            hash->zoneConfStartTime = myGlobals.actTime;
+            break;
+
+        case FC_SWILS_RCA:
+            break;
+        }
+    }
+#endif
+    
+    /* Update Domain Stats */
+    srcDomain = srcAddr->domain;
+    
+    if (srcDomain == FC_ID_SYSTEM_DOMAIN) {
+        if (srcAddr->area == FC_ID_DOMCTLR_AREA) {
+            srcDomain = srcAddr->port; /* This for addr of type FF.FC.<dom> */ 
+        }
+    }
+
+    dstDomain = dstAddr->domain;
+    if (dstDomain == FC_ID_SYSTEM_DOMAIN) {
+        if (dstAddr->area == FC_ID_DOMCTLR_AREA) {
+            dstDomain = dstAddr->port; /* This for addr of type FF.FC.<dom> */ 
+        }
+    }
+
+    if (srcDomain != FC_ID_SYSTEM_DOMAIN) {
+        incrementTrafficCounter (&hash->domainStats[srcDomain].sentBytes, pktlen);
+
+        switch (protocol) {
+        }
+    }
+    
+    if (dstDomain != FC_ID_SYSTEM_DOMAIN) {
+        incrementTrafficCounter (&hash->domainStats[dstDomain].rcvdBytes, pktlen);
+        
+        switch (protocol) {
+        }
+    }
+
+    switch (protocol) {
+    case FC_FTYPE_SWILS:
+        incrementTrafficCounter (&hash->fcSwilsBytes, pktlen);
+        break;
+    case FC_FTYPE_SCSI:
+        incrementTrafficCounter (&hash->fcFcpBytes, pktlen);
+        break;
+    case FC_FTYPE_SBCCS:
+        incrementTrafficCounter (&hash->fcFiconBytes, pktlen);
+        break;
+    case FC_FTYPE_ELS:
+        incrementTrafficCounter (&hash->fcElsBytes, pktlen);
+        break;
+    case FC_FTYPE_FCCT:
+        gs_type = bp[4];
+        gs_stype = bp[5];
+
+        if ((gs_type == FCCT_GSTYPE_DIRSVC) && (gs_stype == FCCT_GSSUBTYPE_DNS)) {
+            incrementTrafficCounter (&hash->fcDnsBytes, pktlen);
+        }
+        else {
+            incrementTrafficCounter (&hash->otherFcBytes, pktlen);
+        }
+        break;
+    case FC_FTYPE_IP:
+        incrementTrafficCounter (&hash->fcIpfcBytes, pktlen);
+        break;
+        
+    default:
+        incrementTrafficCounter (&hash->otherFcBytes, pktlen);
+        break;
+    }
+
+    return (0);
+}
---
+++ fcUtils.h
@@ -0,0 +1,201 @@
+/*
+ * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+ *                          http://www.ntop.org
+ *
+ * Copyright (C) 1998-2002 Luca Deri <deri@ntop.org>
+ *
+ * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __FC_UTILS_H__
+#define __FC_UTILS_H__
+
+#define FC_ID_SYSTEM_DOMAIN    0xFF
+#define FC_ID_DOMCTLR_AREA     0xFC
+#define FC_BROADCAST_ADDR      "ff.ff.ff"
+#define FC_FAB_CTLR_ADDR       "ff.ff.fd"
+
+#define MDSHDR_HEADER_SIZE               16
+#define MDSHDR_TRAILER_SIZE              6
+
+/* SOF Encodings */
+#define MDSHDR_SOFc1                     0x1
+#define MDSHDR_SOFi1                     0x2
+#define MDSHDR_SOFn1                     0x3
+#define MDSHDR_SOFi2                     0x4
+#define MDSHDR_SOFn2                     0x5
+#define MDSHDR_SOFi3                     0x6
+#define MDSHDR_SOFn3                     0x7
+#define MDSHDR_SOFf                      0x8
+#define MDSHDR_SOFc4                     0x9
+#define MDSHDR_SOFi4                     0xa
+#define MDSHDR_SOFn4                     0xb
+
+/* EOF Encodings */
+#define MDSHDR_EOFt                      0x1
+#define MDSHDR_EOFdt                     0x2
+#define MDSHDR_EOFa                      0x4
+#define MDSHDR_EOFn                      0x3
+#define MDSHDR_EOFdti                    0x6
+#define MDSHDR_EOFni                     0x7
+#define MDSHDR_EOFrt                     0xa
+#define MDSHDR_EOFrti                    0xe
+#define MDSHDR_EOF_UNKNOWN               0xb
+
+/* R_CTL upper bits creates a classification tree */
+#define FC_RCTL_DEV_DATA       0x00
+#define FC_RCTL_ELS            0x20
+#define FC_RCTL_LINK_DATA      0x30
+#define FC_RCTL_VIDEO          0x40
+#define FC_RCTL_BLS            0x80
+#define FC_RCTL_LINK_CTL       0xC0
+/* XXX - is 0xF0 Extended Routing?  It is in the FC-FS draft on the T11
+   Web site. */
+
+#define FC_TYPE_CMNSVC         0x0  /* Used in PRLI Svc Param Page */
+
+/* TYPE definitions for Basic or Extended Link_Data */
+#define FC_TYPE_ELS            0x1
+
+/* TYPE definitions for FC-4 */
+#define FC_TYPE_LLCSNAP        0x4
+#define FC_TYPE_IP             0x5
+#define FC_TYPE_SCSI           0x8
+#define FC_TYPE_SB_TO_CU       0x1B /* SB3 Control Unit -> Channel */
+#define FC_TYPE_SB_FROM_CU     0x1C /* SB3 Channel -> Control Unit */ 
+#define FC_TYPE_FCCT           0x20
+#define FC_TYPE_SWILS          0x22
+#define FC_TYPE_AL             0x23
+#define FC_TYPE_SNMP           0x24
+
+/* Derived Frame types (used for ULP demux) */
+#define FC_FTYPE_SWILS         0x1
+#define FC_FTYPE_IP            0x2
+#define FC_FTYPE_SCSI          0x3
+#define FC_FTYPE_BLS           0x4
+#define FC_FTYPE_ELS           0x5
+#define FC_FTYPE_FCCT          0x7
+#define FC_FTYPE_LINKDATA      0x8
+#define FC_FTYPE_VDO           0x9
+#define FC_FTYPE_LINKCTL       0xA
+#define FC_FTYPE_SWILS_RSP     0xB
+#define FC_FTYPE_SBCCS         0xC
+#define FC_FTYPE_UNDEF         0xD
+
+/* Well-known Address Definitions (in Network order) */
+#define FC_WKA_MULTICAST       0xFFFFF5
+#define FC_WKA_CLKSYNC         0xFFFFF6
+#define FC_WKA_KEYDIST         0xFFFFF7
+#define FC_WKA_ALIAS           0xFFFFF8
+#define FC_WKA_QOSF            0xFFFFF9
+#define FC_WKA_MGMT            0xFFFFFA
+#define FC_WKA_TIME            0xFFFFFB
+#define FC_WKA_DNS             0xFFFFFC
+#define FC_WKA_FABRIC_CTRLR    0xFFFFFD
+#define FC_WKA_FPORT           0xFFFFFE
+#define FC_WKA_BCAST           0xFFFFFF
+
+#define FC_ELS_CMD_ACC         0x02
+#define FC_ELS_CMD_PLOGI       0x03
+#define FC_ELS_CMD_LOGO        0x05
+#define FC_ELS_CMD_RSCN          0x61
+#define FC_ELS_CMD_SCR           0x62
+
+/* Well-known GSTYPEs */
+#define FCCT_GSTYPE_KEYSVC   0xF7
+#define FCCT_GSTYPE_ALIASSVC 0xF8
+#define FCCT_GSTYPE_MGMTSVC  0xFA
+#define FCCT_GSTYPE_TIMESVC  0xFB
+#define FCCT_GSTYPE_DIRSVC   0xFC
+
+/* Well-known GSSUBTYPES */
+/* Actual servers serving the directory service type identified by subtype */ 
+#define FCCT_GSSUBTYPE_DNS  0x02
+#define FCCT_GSSUBTYPE_IP   0x03
+#define FCCT_GSSUBTYPE_FCS  0x01
+#define FCCT_GSSUBTYPE_UNS  0x02
+#define FCCT_GSSUBTYPE_FZS  0x03
+#define FCCT_GSSUBTYPE_AS   0x01
+#define FCCT_GSSUBTYPE_TS   0x01
+
+#define FCDNS_RPN_ID   0x0212
+#define FCDNS_RNN_ID   0x0213
+#define FCDNS_RCS_ID   0x0214
+#define FCDNS_RFT_ID   0x0217
+#define FCDNS_RSPN_ID  0x0218
+#define FCDNS_RPT_ID   0x021A
+#define FCDNS_RIPP_ID  0x021B
+#define FCDNS_RHA_ID   0x021D
+#define FCDNS_RFD_ID   0x021E
+#define FCDNS_RFF_ID   0x021F
+#define FCDNS_RIP_NN   0x0235
+#define FCDNS_RIPA_NN  0x0236
+#define FCDNS_RSNN_NN  0x0239
+
+/* Information Categories based on lower 4 bits of R_CTL */
+#define FCP_IU_DATA              0x1
+#define FCP_IU_CONFIRM           0x3
+#define FCP_IU_XFER_RDY          0x5
+#define FCP_IU_CMD               0x6
+#define FCP_IU_RSP               0x7
+
+/* SWILS Commands */
+#define FC_SWILS_SWRJT          0x01
+#define FC_SWILS_SWACC          0x02
+#define FC_SWILS_ELP            0x10
+#define FC_SWILS_EFP            0x11
+#define FC_SWILS_DIA            0x12
+#define FC_SWILS_RDI            0x13
+#define FC_SWILS_HLO            0x14
+#define FC_SWILS_LSU            0x15
+#define FC_SWILS_LSA            0x16
+#define FC_SWILS_BF             0x17
+#define FC_SWILS_RCF            0x18
+#define FC_SWILS_RSCN           0x1B
+#define FC_SWILS_DRLIR          0x1E
+#define FC_SWILS_DSCN           0x20
+#define FC_SWILS_LOOPD          0x21
+#define FC_SWILS_MR             0x22
+#define FC_SWILS_ACA            0x23
+#define FC_SWILS_RCA            0x24
+#define FC_SWILS_SFC            0x25
+#define FC_SWILS_UFC            0x26
+#define FC_SWILS_ESC            0x30
+
+#define MAX_FC_DOMAINS          240
+
+#define DEFAULT_VSAN            1
+#define MAX_USER_VSAN           1001
+#define MAX_VSANS_GRAPHED       10
+#define MAX_VSANS               4095
+
+#define MAX_FC_PROTOCOL_NAME    12
+#define MAX_FC_PROTOCOLS        6
+
+#define FC_HDR_SIZE             24
+
+/* SW_RSCN fields */
+#define FC_SW_RSCN_FABRIC_DETECT 0x01
+#define FC_SW_RSCN_NPORT_DETECT  0x02
+
+#define FC_SW_RSCN_PORT_ONLINE   0x10
+#define FC_SW_RSCN_PORT_OFFLINE  0x20
+
+/* FC Alias Size */
+#define FC_ALIAS_SIZE            64
+
+#endif
