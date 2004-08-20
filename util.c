cc3904ce501b4bb3b171a6bb1c2cce9addd0528a
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2004-08-20 12:54:12 +0000

    Fixed buffer overflow

--- util.c
+++ util.c
@@ -5268,7 +5268,7 @@ static const char *inet_ntop6(const u_char *src, char *dst, size_t size) {
       tp += strlen(tp);
       break;
     }
-    tp += safe_snprintf(__FILE__, __LINE__, tp, sizeof(tp), "%x", words[i]);
+    tp += safe_snprintf(__FILE__, __LINE__, tp, sizeof(tmp), "%x", words[i]);
   }
 
   /* Was it a trailing run of 0x00's? */
