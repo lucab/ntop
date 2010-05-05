7d17e1cb9a546431f5e0a38df6ce3ee06f27d0c8
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2010-05-05 11:11:56 +0000

    minor changes

--- python/json/iphone/ifdata.py
+++ python/json/iphone/ifdata.py
@@ -84,7 +84,7 @@ try:
 				adapter['peak'] = {'packets': rs.pop('peakPkts',''), 'bytes': rs.pop('peakBytes','')}
 				adapter['last minute'] = {'packets': rs.pop('lastMinPkts',''), 'bytes': rs.pop('lastMinBytes','')}
 				adapter['last 5 minutes'] = {'packets': rs.pop('lastFiveMinsPkts',''), 'bytes': rs.pop('lastFiveMinsBytes','')}
-				if (len(rs) > 0) adapter['other'] = rs
+				if (len(rs) > 0): adapter['other'] = rs
 				
 				throughputStats = { "metadata": { "type":"groupedtable", "title":"Interface Throughput", "unit": "bps", "mode" : "gauge", "total": total }, "data" : adapter }
 			
